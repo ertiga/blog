48b985b9e10316f2a6448c7a109a28c9ca5dd89b
Rhian Rizki Ramadhan <r3g99devilangel@yahoo.com>
2018-01-12 22:23:06 +0700

    uas

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,4 @@
 <?php
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -10,11 +9,33 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::get('hai', function(){
+	return 'Hai ini route hai';
+});
+Route::get('hello', function(){
+	return view('Hello');
+});
+Route::get('haihai', 'HelloController@test' );
+Route::get('pengguna', function(){
+	return App\User::all();
+});
+Route::resource('category', 'CategoryController');
+Route::resource('post', 'PostController');
+Route::resource('author', 'AuthorController');
+Route::resource('publisher', 'PublisherController');
+Route::resource('book', 'BookController');
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function(){
+	Route::resource('category', 'CategoryController');
+	Route::resource('post', 'PostController');
+	Route::resource('author', 'AuthorController');
+	Route::resource('publisher', 'PublisherController');
+	Route::resource('book', 'BookController');
+	Route::get('/', function() {
+		return view('welcome');
+		});
+	});
