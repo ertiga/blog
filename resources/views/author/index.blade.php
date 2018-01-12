3d19c34997c916e5464d145ba51fd62460341fef
Rhian Rizki Ramadhan <r3g99devilangel@yahoo.com>
2018-01-12 22:14:13 +0700

    uas

--- resources/views/author/index.blade.php
+++ resources/views/author/index.blade.php
@@ -5,24 +5,24 @@
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <div class="panel panel-default">
-                <div class="panel-heading">Category
-                <a href="{{route('category.create')}}" class="btn btn-sm btn-primary"> + </a>
+                <div class="panel-heading">Authors
+                <a href="{{route('author.create')}}" class="btn btn-sm btn-primary"> + </a>
                 </div>
                 <div class="panel-body">
                     <table class="table">
                         <tr>
                         <th>No</th>
-                        <th>Nama Kategori</th>
+                        <th>Nama Author</th>
                         <th>Tindakan</th>
                         </tr>
-                        @forelse($categories as $category)
+                        @forelse($authors as $author)
                         <tr>
-                        <td>{{$category->id}}</td>
-                        <td>{{$category->name}}</td>
-                        <td><a href="{{route('category.edit', $category->id)}}" class="btn btn-sm btn-warning">Edit</a>
-                        <a href="{{route('category.show', $category->id)}}" class="btn btn-sm btn-success">Show</a>
+                        <td>{{$author->id}}</td>
+                        <td>{{$author->name}}</td>
+                        <td><a href="{{route('author.edit', $author->id)}}" class="btn btn-sm btn-warning">Edit</a>
+                        <a href="{{route('author.show', $author->id)}}" class="btn btn-sm btn-success">Show</a>
                         </td>
-                        <td><form action="{{route('category.destroy', $category->id)}}" method="post">
+                        <td><form action="{{route('author.destroy', $author->id)}}" method="post">
                         {{ csrf_field() }}
                         {{method_field('DELETE')}}
                         <input type="submit" value="Delete" class="btn btn-sm btn-danger">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
                 <div class="navbar-header">
 
                     <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
                         <span class="sr-only">Toggle Navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
@@ -36,22 +36,26 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                        <li><a href="{{route('category.index')}}">Categories</a></li>
+                        <li><a href="{{route('post.index')}}">Posts</a></li>
+                        <li><a href="{{route('author.index')}}">Authors</a></li>
+                        <li><a href="{{route('publisher.index')}}">Publishers</a></li>
+                        <li><a href="{{route('book.index')}}">Books</a></li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
                         <!-- Authentication Links -->
-                        @guest
+                        @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
                             <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
                                 </a>
 
-                                <ul class="dropdown-menu">
+                                <ul class="dropdown-menu" role="menu">
                                     <li>
                                         <a href="{{ route('logout') }}"
                                             onclick="event.preventDefault();
@@ -65,7 +69,7 @@
                                     </li>
                                 </ul>
                             </li>
-                        @endguest
+                        @endif
                     </ul>
                 </div>
             </div>
